90ad6d13a48416a18743d6cf4f10afb5529f927e
Om Chokse <omchokse30@gmail.com>
2025-03-20 17:17:42 +0530

    UC2_Email

---
+++ UC2_Email.js
@@ -0,0 +1,15 @@
+const validateEmail = (email) => {
+    const emailRegex = /^abc(\.[a-zA-Z]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
+    return emailRegex.test(email);
+};
+
+console.log(validateEmail("abc@bridgelabz.co"));       // true
+console.log(validateEmail("abc.xyz@bridgelabz.co"));   // true
+console.log(validateEmail("abc@bridgelabz.co.in"));    // true
+console.log(validateEmail("abc.xyz@bridgelabz.co.in"));// true
+console.log(validateEmail("abc@xyz.co"));             // false
+console.log(validateEmail("abc@bridgelabzcom"));      // false
+console.log(validateEmail("abc@bridgelabz."));        // false
+console.log(validateEmail("abc@bridgelabz.co.123"));  // false
+console.log(validateEmail("abc@bridgelabz"));         // false
+console.log(validateEmail("abc@bridgelabz.co..in"));  // false
