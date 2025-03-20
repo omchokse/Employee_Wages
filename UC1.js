e45ce1e8a58801e2db37d51d06708a3496867801
Om Chokse <omchokse30@gmail.com>
2025-03-20 14:51:16 +0530

    Check emp is present

---
+++ UC1.js
@@ -0,0 +1,8 @@
+const Isabsent=0
+let emp=Math.floor(Math.random()*10)%2;
+if(emp==Isabsent){
+    console.log("Employee is Absent");
+    return;
+}else{
+    console.log("Employee is Present");
+}
