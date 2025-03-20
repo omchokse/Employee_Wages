2b34a9fdc5a6de0d3dc3c50bf2285bff65c5dd7f
Om Chokse <omchokse30@gmail.com>
2025-03-20 17:18:11 +0530

    UC4_Email

---
+++ UC4_Email.js
@@ -0,0 +1,18 @@
+const validateEmail = (email) => {
+    const emailRegex = /^abc([._+,-][a-zA-Z]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
+    return emailRegex.test(email);
+};
+
+console.log(validateEmail("abc@bridgelabz.co"));         // true
+console.log(validateEmail("abc.xyz@bridgelabz.co"));     // true
+console.log(validateEmail("abc_xyz@bridgelabz.co.in"));  // true
+console.log(validateEmail("abc+xyz@bridgelabz.co.in"));  // true
+console.log(validateEmail("abc-xyz@bridgelabz.co.in"));  // true
+console.log(validateEmail("abc,xyz@bridgelabz.co.in"));  // true
+console.log(validateEmail("abc#xyz@bridgelabz.co.in"));  // false
+console.log(validateEmail("abc@bridgelabzcom"));        // false
+console.log(validateEmail("abc@bridgelabz"));           // false
+console.log(validateEmail("abc@bridgelabz.co.123"));    // false
+console.log(validateEmail("abc@bridgelabz..co"));       // false
+console.log(validateEmail("abc.@bridgelabz.co.in"));    // false
+console.log(validateEmail("abc..xyz@bridgelabz.co"));   // false
